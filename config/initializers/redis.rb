b9f235dc98bd38a51f56c624705cbd5346ddbb3c
Evan Kuchar <evan.kuchar@gmail.com>
2013-07-10 11:45:08 -0700

    config redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,6 @@
-$redis = Redis.new
+if Rails.env.development?
+  $redis = Redis.new
+else
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
